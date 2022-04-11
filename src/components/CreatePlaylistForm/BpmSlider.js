6d24bcd8af2d8971bcef568f3e2a42a49e82941c
tessaRuckstuhl <tessa.ruckstuhl@web.de>
2022-04-11 17:32:10 +0200

    remove proxy, fix lint warnings

--- src/components/CreatePlaylistForm/BpmSlider.js
+++ src/components/CreatePlaylistForm/BpmSlider.js
@@ -1,4 +1,4 @@
-import { Slider, Stack } from '@mui/material';
+import { Slider } from '@mui/material';
 import React from 'react';
 function BpmSlider(props) {
   const { value, onChange, name } = props;
--- src/components/CreatePlaylistForm/CreateBtn.js
+++ src/components/CreatePlaylistForm/CreateBtn.js
@@ -1,7 +1,5 @@
-import axios from 'axios';
 import { Button } from '@mui/material';
 import React from 'react';
-import PlaylistService from '../../services/PlaylistService';
 import { useToken } from '../../contexts/TokenContext';
 import assembleNewPlaylist from '../../utils/assembleNewPlaylist';
 import createPlaylistFromUris from '../../utils/createPlaylistFromUris';
