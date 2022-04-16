4b9beb78d4d18edd0a0ef59bbbc8f759b50bfc82
tessaRuckstuhl <tessa.ruckstuhl@web.de>
2022-04-16 16:00:48 +0200

    update login text

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,12 @@ import LoginBtn from '../components/Login/LoginBtn';
 function Landing() {
   return (
     <div className="flex justify-center ">
-      <div className=" flex w-5/6 flex-col sm:w-1/3">
-        <div className="mb-12  font-londrinaSolid text-3xl sm:text-5xl">
+      <div className=" flex w-5/6 flex-col text-center sm:w-1/3">
+        <div className="mb-6 font-londrinaShadow text-3xl sm:text-5xl">
           Create your playlist for running.
+        </div>
+        <div className="text-1xl  mb-12 font-londrinaSolid sm:text-3xl">
+          Login to get started.
         </div>
         <LoginBtn />
       </div>
