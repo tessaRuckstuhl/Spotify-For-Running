a2718e52ba84d396f5063e208ea6273ca436f20e
tessaRuckstuhl <tessa.ruckstuhl@web.de>
2022-04-16 13:41:19 +0200

    update index for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import LoginBtn from '../components/Login/LoginBtn';
 function Landing() {
   return (
     <div className="flex justify-center ">
-      <div className=" flex w-1/3 flex-col">
-        <div className="mb-12  font-londrinaSolid text-5xl">
+      <div className=" flex w-5/6 sm:w-1/3 flex-col">
+        <div className="mb-12  font-londrinaSolid sm:text-5xl text-3xl">
           Create your playlist for running.
         </div>
         <LoginBtn />
